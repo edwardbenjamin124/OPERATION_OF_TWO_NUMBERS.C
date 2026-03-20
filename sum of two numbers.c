e2ed06b13ed42dca99043702b795326c2a7524ef
edwardbenjamin124 <edwardbenjamin124@gmail.com>
2026-03-20 22:18:03 +0300

    Add files via upload

---
+++ sum of two numbers.c
@@ -0,0 +1,19 @@
+           //my first programin c language
+#include<stdio.h>
+int main()
+{  
+           //input
+	float num1,num2;
+	float sum;
+	
+	//enter the input
+	printf("ENTER THE TWO NUMBERS\n");
+	scanf("%f,%f",&num1,&num2);
+	
+	//decision
+	sum=num1+num2;
+	
+	//output
+	printf("the sum of two numbers is %f",sum);
+	
+}
