3bd23ef3af4468b16f3e488eebff07fc45d30d69
edwardbenjamin124 <edwardbenjamin124@gmail.com>
2026-03-20 22:29:18 +0300

    Add files via upload

---
+++ subtraction of two numbers.c
@@ -0,0 +1,18 @@
+           //my second programin c language
+#include<stdio.h>
+int main()
+{  
+           //input
+	float num1,num2;
+	float sum;
+	
+	//enter the input
+	printf("enter the two numbers\n");
+	scanf("%f,%f",&num1,&num2);
+	
+	//decision
+	 sum=num1-num2;
+	
+	//output
+	printf("the sum of subtraction of two numbers is %f",sum);
+}
