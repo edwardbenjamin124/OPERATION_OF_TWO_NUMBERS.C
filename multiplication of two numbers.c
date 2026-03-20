b45524c3cecfb149e5aa750f901ae80ae57df556
edwardbenjamin124 <edwardbenjamin124@gmail.com>
2026-03-20 22:35:28 +0300

    Add files via upload

---
+++ multiplication of two numbers.c
@@ -0,0 +1,18 @@
+           //my third program in c language
+#include<stdio.h>
+int main()
+{  
+           //input
+	float num1,num2;
+	float product;
+	
+	//enter the input
+	printf("enter the two numbers\n");
+	scanf("%f,%f",&num1,&num2);
+	
+	//decision
+	 product=num1/num2;
+	
+	//output
+	printf("the product of two numbers is %f",product);
+}
