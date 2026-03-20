da06c4fc6ea724bc5b85be41f127b6353bb83fff
edwardbenjamin124 <edwardbenjamin124@gmail.com>
2026-03-20 22:43:52 +0300

    Add files via upload

---
+++ division of two numbers.cpp
@@ -0,0 +1,18 @@
+           //my third program in c language
+#include<stdio.h>
+int main()
+{  
+           //input
+	float num1,num2;
+	float quotient;
+	
+	//enter the input
+	printf("enter the two numbers\n");
+	scanf("%f,%f",&num1,&num2);
+	
+	//decision
+	 quotient=num1/num2;
+	
+	//output
+	printf("the quotient  of two numbers is %f",quotient);
+}
